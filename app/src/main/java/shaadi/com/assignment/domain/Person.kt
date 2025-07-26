d28d001655eff77b2a487ceb3b56e5c8d31f464a
rahul <ra9853044@gmail.com>
2025-07-26 22:07:49 +0530

    person data class for recyclerview adapter

---
+++ app/src/main/java/shaadi/com/assignment/domain/Person.kt
@@ -0,0 +1,12 @@
+package shaadi.com.assignment.domain
+
+
+data class Person(
+    val id:Int,
+    val uuid:String,
+    val name:String,
+    val age:Int,
+    val address:String,
+    val image:String,
+    val status:String,
+)
